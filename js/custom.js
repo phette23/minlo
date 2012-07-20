8cf106694c919c472d4d558d1f95210d0b9a3969
phette23 <phette23@gmail.com>
2012-07-20 18:56:34 -0400

    breakout custom JS into separate file

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+(function( $ ) {
+    $( document ).bind( "mobileinit", function(){
+        // change default page transition to slide, looks better than fade
+        $.mobile.defaultPageTransition = "slide";
+
+        // see more global configuration options:
+        // http://jquerymobile.com/demos/1.1.1/docs/api/globalconfig.html
+        });
+    }( jQuery ));
